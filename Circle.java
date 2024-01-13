7a48d01291e930c97a96e039625c92b95a65f494
Sujan <120612188+sujan-22@users.noreply.github.com>
2024-01-12 23:41:56 -0500

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -1,24 +1,42 @@
-//Author: Sujan Rokad, 000882948
+/*
+ * Author:                 Sujan Rokad, 000882948
+ * Authorship statement:   I, Sujan Rokad, 000882948, certify that this material is my original work.
+ *                         No other person's work has been used without due acknowledgment.
+ * Purpose:                Define a class representing a circle that can be drawn on a canvas.
+ */
 
 package Assignment_000882948;
 
 import javafx.scene.canvas.GraphicsContext;
 
-// This class represents a circle that can be drawn on a canvas using the JavaFX GraphicsContext.
-public class Circle extends GeometricObject{
+/**
+ * The Circle class represents a circle that can be drawn on a canvas using the JavaFX GraphicsContext.
+ *
+ * @author Sujan Rokad
+ */
+public class Circle extends GeometricObject {
     private double radius; // radius of the circle
 
-    // Constructor that sets the initial x and y coordinates of the circle and its radius
+    /**
+     * Constructs a Circle object with the specified initial x and y coordinates and radius.
+     *
+     * @param x      The initial x-coordinate.
+     * @param y      The initial y-coordinate.
+     * @param radius The radius of the circle.
+     */
     public Circle(double x, double y, double radius) {
         super(x, y); // Call the constructor of the superclass (GeometricObject) to set x and y coordinates
         this.radius = radius; // Set the radius of the circle
     }
 
-    // Method that must be implemented as part of the Drawable interface
-    // Draws the circle on a canvas using the given GraphicsContext
+    /**
+     * Draws the circle on a canvas using the given GraphicsContext.
+     *
+     * @param gc The GraphicsContext used for drawing.
+     */
     @Override
-    public void draw(GraphicsContext gc){
+    public void draw(GraphicsContext gc) {
         // The x and y coordinates are adjusted to position the center of the circle at (x, y)
-        gc.fillOval(getX() - radius, getY() - radius, 3*radius, 3*radius);
+        gc.fillOval(getX() - radius, getY() - radius, 3 * radius, 3 * radius);
     }
 }
