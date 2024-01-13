60170093b95762b6934a4eb034341a893c05c9e1
Sujan <120612188+sujan-22@users.noreply.github.com>
2024-01-12 23:42:23 -0500

    Update Drawable.java

--- Drawable.java
+++ Drawable.java
@@ -1,12 +1,24 @@
-//Author: Sujan Rokad, 000882948
+/*
+ * Author:                 Sujan Rokad, 000882948
+ * Authorship statement:   I, Sujan Rokad, 000882948, certify that this material is my original work.
+ *                         No other person's work has been used without due acknowledgment.
+ * Purpose:                Define an interface for objects that can be drawn on a canvas using the JavaFX GraphicsContext.
+ */
 
 package Assignment_000882948;
 
 import javafx.scene.canvas.GraphicsContext;
 
-// This interface defines a contract for objects that can be drawn on a canvas using the JavaFX GraphicsContext.
+/**
+ * The Drawable interface defines a contract for objects that can be drawn on a canvas using the JavaFX GraphicsContext.
+ *
+ * @author Sujan Rokad
+ */
 public interface Drawable {
-    // Method that must be implemented by classes that implement the Drawable interface
-    // Draws the object on a canvas using the given GraphicsContext
+    /**
+     * Draws the object on a canvas using the given GraphicsContext.
+     *
+     * @param gc The GraphicsContext used for drawing.
+     */
     void draw(GraphicsContext gc);
 }
